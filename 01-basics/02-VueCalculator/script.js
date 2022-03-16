793107c1be01bddb09dce934efbfaf32bf8c680f
Ilya <sidorovilya94@mail.ru>
2022-03-17 01:12:19 +0500

    Completed task 02-VueCalculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,28 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const app = createApp({
+    data() {
+        return {
+            numOne: null,
+            numTwo: null,
+            operation: 'sum'
+        }
+    },
+    computed: {
+        result() {
+            switch (this.operation) {
+                case 'sum':
+                    return this.numOne + this.numTwo;                    
+                case 'subtract':
+                    return this.numOne - this.numTwo;                   
+                case 'multiply':
+                    return this.numOne * this.numTwo;                    
+                case 'divide':
+                    return this.numOne / this.numTwo;
+                default:
+                    return 0;                 
+            }
+        }
+    }
+});
+app.mount('#app');
