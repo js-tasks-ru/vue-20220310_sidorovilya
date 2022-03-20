d2d8373ee91bfdfad8cb861ad2ecda33890aaae4
Ilya <sidorovilya94@mail.ru>
2022-03-20 17:52:01 +0500

    Completed task 01-CounterButtonComponent

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -4,9 +4,19 @@ export default defineComponent({
   name: 'CounterButton',
 
   // Компонент должен иметь входной параметр
-
+  props: {
+    count: {
+      type: Number,
+      default: 0
+    }
+  },
   // Шаблон лучше держать максимально простым, а логику выносить в методы
-
+  methods: {
+    click() {
+      let count = this.count ?  this.count : 0;
+      this.$emit('update:count', ++count);
+    }
+  },
   // Шаблон потребуется отредактировать
-  template: `<button type="button">1</button>`,
+  template: `<button type="button" @click="click">{{ count }}</button>`,
 });
