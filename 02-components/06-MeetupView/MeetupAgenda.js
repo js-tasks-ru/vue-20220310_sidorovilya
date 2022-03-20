fb9683e06345b86bfc805422a942e736e326eed1
Ilya <sidorovilya94@mail.ru>
2022-03-20 18:03:23 +0500

    06-MeetupView

---
+++ 02-components/06-MeetupView/MeetupAgenda.js
@@ -0,0 +1,21 @@
+import { defineComponent } from './vendor/vue.esm-browser.js';
+import MeetupAgendaItem from './MeetupAgendaItem.js';
+
+export default defineComponent({
+  name: 'MeetupAgenda',
+  components: {
+    MeetupAgendaItem,
+  },
+  props: {
+    agenda: {
+      type: Array,
+      required: true
+    }
+  },
+  template: `
+    <ul class="agenda">
+      <li class="agenda__item" v-for="agendaItem in agenda">
+        <meetup-agenda-item :agendaItem="agendaItem"></meetup-agenda-item>
+      </li>
+    </ul>`,
+});
