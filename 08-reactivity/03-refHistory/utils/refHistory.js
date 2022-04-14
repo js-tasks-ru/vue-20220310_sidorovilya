194437250eaf6a4d19adf0eb851f64b8baf1986d
Ilya <sidorovilya94@mail.ru>
2022-04-14 22:15:25 +0500

    Completed task 03-refHistory

--- 08-reactivity/03-refHistory/utils/refHistory.js
+++ 08-reactivity/03-refHistory/utils/refHistory.js
@@ -1,12 +1,17 @@
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 
 /**
  * @template T
  * @param {Ref<T>} source - Отслеживаемый ref
  * @returns {Object<{ history: Ref<T[]> }>} - История изменения source
  */
-export function refHistory(source) {
-  const history = ref();
-  // ...
-  return { history };
+export function refHistory(source) {  
+  const history = ref([]);
+  watch(source, (newValue) => {
+    history.value.push(newValue);
+  }, {
+    immediate: true,
+    flush: 'sync'
+  });
+  return { history };    
 }
